2fdf9afb312548ace0489a0ce36ac47ec9980186
yuzatea510 <165344560+yuzatea510@users.noreply.github.com>
2024-05-10 17:02:47 +0900

    NAV

--- Projects/KidsCare_UI/src/main/java/kr/ac/kopo/kidscare/config/SecurityConfig.java
+++ Projects/KidsCare_UI/src/main/java/kr/ac/kopo/kidscare/config/SecurityConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
