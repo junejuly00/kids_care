8cac95704dd2a8e86ebde39a36e9beee4ef11dc8
hyunjung0925 <yuntajan@naver.com>
2024-05-22 18:42:50 +0900

    2024-5-22 ㅇㅎㅈ
    ㅁㅇㄻㄴㅇ

--- Projects/KidsCare/src/main/java/kr/ac/kopo/kidscare/service/NoticeService.java
+++ Projects/KidsCare/src/main/java/kr/ac/kopo/kidscare/service/NoticeService.java
@@ -3,10 +3,11 @@ package kr.ac.kopo.kidscare.service;
 import java.util.List;
 
 import kr.ac.kopo.kidscare.model.Notice;
+import kr.ac.kopo.kidscare.pager.Pager;
 
 public interface NoticeService {
 
-	List<Notice> list();
+	List<Notice> list(Pager pager);
 
 	void add(Notice item);
 
