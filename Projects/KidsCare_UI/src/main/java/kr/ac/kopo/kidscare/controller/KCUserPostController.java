60d74c53b339bcda6cf0c47b3e042b805983d17a
junejuly00 <ayjune02@gmail.com>
2024-05-28 11:40:29 +0900

    아이디 자동으로 들어가게

--- Projects/KidsCare_UI/src/main/java/kr/ac/kopo/kidscare/controller/KCUserPostController.java
+++ Projects/KidsCare_UI/src/main/java/kr/ac/kopo/kidscare/controller/KCUserPostController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -80,6 +82,10 @@ public class KCUserPostController {
 	
 	@PostMapping("/add")
 	String add(KCUserPost item, List<MultipartFile> uploadFile) throws JsonProcessingException {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String cUName = auth.getName();
+		item.setUsername(cUName);
 		
 		List<UserFile> userFiles = new ArrayList<UserFile>();
 		
