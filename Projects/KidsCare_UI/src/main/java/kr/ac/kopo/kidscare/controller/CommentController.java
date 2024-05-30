c387e161865788ee4cc624bdb3b9bae1b7b3ce4c
yuzatea510 <165344560+yuzatea510@users.noreply.github.com>
2024-05-30 20:22:08 +0900

    2024 05 30 JY
    ASDF

--- Projects/KidsCare_UI/src/main/java/kr/ac/kopo/kidscare/controller/CommentController.java
+++ Projects/KidsCare_UI/src/main/java/kr/ac/kopo/kidscare/controller/CommentController.java
@@ -28,84 +28,84 @@ import kr.ac.kopo.kidscare.model.Comment;
 @Controller
 @RequestMapping("/comment")
 public class CommentController {
-	
-	final String url = "http://localhost:9090/comment/";
-	
-	@Autowired
-	private RestTemplate rest = new RestTemplate();
-	
-	@Autowired
-	private ObjectMapper om = new ObjectMapper();
-	
-	@GetMapping("/list")
-	String list(Model model) throws JsonMappingException, JsonProcessingException {
-		
-		HttpHeaders header = new HttpHeaders();
-		header.setContentType(MediaType.APPLICATION_JSON);
-		
-		String resp = rest.getForObject(url + "list", String.class);
-		
+   
+   final String url = "http://localhost:9090/comment/";
+   
+   @Autowired
+   private RestTemplate rest = new RestTemplate();
+   
+   @Autowired
+   private ObjectMapper om = new ObjectMapper();
+   
+   @GetMapping("/list")
+   String list(Model model) throws JsonMappingException, JsonProcessingException {
+      
+      HttpHeaders header = new HttpHeaders();
+      header.setContentType(MediaType.APPLICATION_JSON);
+      
+      String resp = rest.getForObject(url + "list", String.class);
+      
 
-		List<Comment> list = om.readValue(resp, new TypeReference<List<Comment>>() {});
-		
-		
-		model.addAttribute("list",list);	
-		
-		return url + "list";
-	}
-	
-	@PostMapping("/add")
-	String add(Comment commentInfo) throws JsonProcessingException {
-		
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String cUName = auth.getName();
-		commentInfo.setUsername(cUName);
-		
-		HttpHeaders header = new HttpHeaders();
-		header.setContentType(MediaType.APPLICATION_JSON);
-		
-		String jsonString = om.writeValueAsString(commentInfo);
-		
-		HttpEntity<String> req = new HttpEntity<String>(jsonString, header);
-		
-		ResponseEntity<Integer> resp = rest.postForEntity(url + "add", req, Integer.class);
-		
-		Integer body = resp.getBody();
-		
-		System.out.println(body);
-		String postId = commentInfo.getUserPostId().toString();
-		
-		return "redirect:../kcuserpost/post/" + postId;
-	}
-		
-		@GetMapping("/update/{username}")
-		String update(@PathVariable String username, Model model) {
-		Comment commentInfo = rest.getForObject(url + username, Comment.class);
-			
-		model.addAttribute("commentInfo", commentInfo);
-			
-		return "comment/update";
+      List<Comment> list = om.readValue(resp, new TypeReference<List<Comment>>() {});
+      
+      
+      model.addAttribute("list",list);   
+      
+      return url + "list";
+   }
+   
+   @PostMapping("/add")
+   String add(Comment commentInfo) throws JsonProcessingException {
+      
+      Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+      String cUName = auth.getName();
+      commentInfo.setUsername(cUName);
+      
+      HttpHeaders header = new HttpHeaders();
+      header.setContentType(MediaType.APPLICATION_JSON);
+      
+      String jsonString = om.writeValueAsString(commentInfo);
+      
+      HttpEntity<String> req = new HttpEntity<String>(jsonString, header);
+      
+      ResponseEntity<Integer> resp = rest.postForEntity(url + "add", req, Integer.class);
+      
+      Integer body = resp.getBody();
+      
+      System.out.println(body);
+      String postId = commentInfo.getUserPostId().toString();
+      
+      return "redirect:../kcuserpost/post/" + postId;
+   }
+      
+      @GetMapping("/update/{username}")
+      String update(@PathVariable String username, Model model) {
+      Comment commentInfo = rest.getForObject(url + username, Comment.class);
+         
+      model.addAttribute("commentInfo", commentInfo);
+         
+      return "comment/update";
 
-	}
-		
-		@PostMapping("/update/{username}")
-		String update(@PathVariable String username, Comment sitterInfo) throws JsonProcessingException {
-			sitterInfo.setUsername(username);
-			
-			HttpHeaders header = new HttpHeaders();
-			header.add("Content-Type", "application/json");
-			
-			String jsonString = om.writeValueAsString(sitterInfo);
-			
-			HttpEntity<String> req = new HttpEntity<String>(jsonString, header);
-			
-			ResponseEntity<Integer> resp = rest.exchange(url + username, HttpMethod.PUT, req, Integer.class);
-			
-			Integer result = resp.getBody();
-			
-			System.out.println(result);
-			
-			return "redirect:../list";	
-		}
-		
+   }
+      
+      @PostMapping("/update/{username}")
+      String update(@PathVariable String username, Comment sitterInfo) throws JsonProcessingException {
+         sitterInfo.setUsername(username);
+         
+         HttpHeaders header = new HttpHeaders();
+         header.add("Content-Type", "application/json");
+         
+         String jsonString = om.writeValueAsString(sitterInfo);
+         
+         HttpEntity<String> req = new HttpEntity<String>(jsonString, header);
+         
+         ResponseEntity<Integer> resp = rest.exchange(url + username, HttpMethod.PUT, req, Integer.class);
+         
+         Integer result = resp.getBody();
+         
+         System.out.println(result);
+         
+         return "redirect:../list";   
+      }
+      
 }
--- Projects/KidsCare_UI/src/main/java/kr/ac/kopo/kidscare/controller/RootController.java
+++ Projects/KidsCare_UI/src/main/java/kr/ac/kopo/kidscare/controller/RootController.java
@@ -3,7 +3,6 @@ package kr.ac.kopo.kidscare.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.http.HttpSession;
